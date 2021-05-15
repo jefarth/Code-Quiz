e0a65eed05f1ad23c4e9a63626ccfba6cfb788c2
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-05-15 16:19:12 -0600

    Added psuedo-code

---
+++ assests/js/script.js
@@ -0,0 +1,9 @@
+// Create body elements - header, body, lists, paragraphs
+// Create a start button
+// Show new page with questions when start button is clicked
+// Create a count-down timer that activates when start button is clicked
+// Create array to hold questions
+// Create a function to subtract time when answers are incorrect
+// End game when all questions answered or timer hits 0
+// Create a function to keep track of score
+// User can save initials with score after game over
